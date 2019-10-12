abdfcfe9f946373531f5310e896c53de7c52f849
C1491 <zhoufeifei@hanbell.cn>
2019-10-12 08:18:35 +0800

    REPMQBean更新
    REPMQBean更新

--- CRM-ejb/src/java/cn/hanbell/crm/ejb/REPMQBean.java
+++ CRM-ejb/src/java/cn/hanbell/crm/ejb/REPMQBean.java
@@ -16,7 +16,7 @@ import javax.ejb.LocalBean;
  */
 @Stateless
 @LocalBean
-public class REPMQBean extends SuperEJBForCRM{
+public class REPMQBean extends SuperEJBForCRM<REPMQ>{
 
     public REPMQBean() {
         super(REPMQ.class);
