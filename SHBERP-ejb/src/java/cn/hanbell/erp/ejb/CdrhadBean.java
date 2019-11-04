cbc563098aeacc0a9f3ad0d4ab73d64db14b2626
C1879 <40564060+C1879@users.noreply.github.com>
2019-11-04 09:32:16 +0800

    销售时间查询、客诉代码修改

--- SHBERP-ejb/src/java/cn/hanbell/erp/ejb/CdrhadBean.java
+++ SHBERP-ejb/src/java/cn/hanbell/erp/ejb/CdrhadBean.java
@@ -9,6 +9,7 @@ import cn.hanbell.erp.comm.SuperEJBForERP;
 import cn.hanbell.erp.entity.Cdrdta;
 import cn.hanbell.erp.entity.Cdrhad;
 import cn.hanbell.erp.entity.Cdrlot;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -44,6 +45,46 @@ public class CdrhadBean extends SuperEJBForERP<Cdrhad> {
         } catch (Exception ex) {
             return null;
         }
+    }
+
+    public List<String[]> queryShpdateList(String facno, String[] itnbrs) {
+        List<String[]> shpdateList = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < itnbrs.length; i++) {
+            String itnbr = itnbrs[i].trim();
+            String[] arr = new String[2];
+            arr[0] = itnbr;
+            if (!"".equals(itnbr)) {
+                sb.setLength(0);
+                sb.append(" SELECT top 1 convert(varchar(10),shpdate,111) FROM cdrhadh h,cdrdtah d where h.shpno=d.shpno and d.facno= '${facno}' and d.itnbr =  '${itnbr}' ORDER BY shpdate ASC ");
+                String sql1 = sb.toString().replace("${itnbr}", itnbr).replace("${facno}", facno);
+                sb.setLength(0);
+                sb.append(" SELECT top 1 convert(varchar(10),shpdate,111)  FROM cdrhad h,cdrdta d   where h.shpno=d.shpno and d.facno= '${facno}' and d.itnbr =  '${itnbr}'  ORDER BY shpdate ASC ");
+                String sql2 = sb.toString().replace("${itnbr}", itnbr).replace("${facno}", facno);
+                try {
+                    setCompany(facno);
+                    Query query1 = getEntityManager().createNativeQuery(sql1);
+                    List result;
+                    //第一次查询销售历史表数据，如无记录则查询现销售表数据赋值
+                    result = query1.getResultList();
+                    if (result != null && !result.isEmpty()) {
+                        arr[1] = result.get(0).toString();
+                    } else {
+                        Query query2 = getEntityManager().createNativeQuery(sql2);
+                        result  =query2.getResultList();
+                        if (result != null && !result.isEmpty()) {
+                            arr[1] = result.get(0).toString();
+                        } else {
+                            arr[1] = "null";
+                        }
+                    }
+                } catch (Exception e) {
+                    arr[1] = e.toString();
+                }
+                shpdateList.add(arr);
+            }
+        }
+        return shpdateList;
     }
 
     public List<Cdrlot> findCdrlotList(String facno, String shpno) {
