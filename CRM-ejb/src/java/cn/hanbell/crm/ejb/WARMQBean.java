ca21ec4f614666ffe0b501e25082031033bf2a95
沈鑫_SHB_资讯室 <C2082@hanbell.com.cn>
2020-07-09 16:08:53 +0800

    修改SQL，移除sql中的库名

--- CRM-ejb/src/java/cn/hanbell/crm/ejb/WARMQBean.java
+++ CRM-ejb/src/java/cn/hanbell/crm/ejb/WARMQBean.java
@@ -25,7 +25,7 @@ public class WARMQBean extends SuperEJBForCRM<WARMQ>{
         super(WARMQ.class);
     }
       public List<Object[]> findByMQ003(String MQ001) {
-        StringBuffer sql=new StringBuffer("SELECT  TOP 50 * FROM ( Select DISTINCT MQ001,MQ002 from CRMDB..WARMQ WARMQ Left join CRMDB..WARMU  as WARMU on MQ001=MU001 ");
+        StringBuffer sql=new StringBuffer("SELECT  TOP 50 * FROM ( Select DISTINCT MQ001,MQ002 from WARMQ WARMQ Left join WARMU  as WARMU on MQ001=MU001 ");
         sql.append("Where MQ003 = N'b1'   and (( MU003 = N'CRMDS' and MQ010='Y' )  or MQ010='N' ) ");
         if(MQ001!=null&&!"".equals(MQ001)){
         sql.append(" AND MQ001='").append(MQ001).append("'");
