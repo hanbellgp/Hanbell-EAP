192904090616faa8dccf6c2e60504450f9ba8241
沈鑫_SHB_资讯室 <C2082@hanbell.com.cn>
2020-10-29 12:04:23 +0800

    2020/10/29 企业微信推送消息和薪资回执是实体类修改

--- EAP-ejb/src/java/cn/hanbell/eap/entity/SalarySend.java
+++ EAP-ejb/src/java/cn/hanbell/eap/entity/SalarySend.java
@@ -5,16 +5,17 @@
  */
 package cn.hanbell.eap.entity;
 
-import java.io.Serializable;
+import com.lightshell.comm.SuperEntity;
 import java.util.Date;
+import javax.persistence.Basic;
 import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -27,28 +28,43 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "SalarySend.findAll", query = "SELECT s FROM SalarySend s"),
-     @NamedQuery(name = "SalarySend.findByTaskidAndDeptno", query = "SELECT s FROM SalarySend s WHERE s.salarySendPK.taskid like :taskid AND s.deptno = :deptno"),
-      @NamedQuery(name = "SalarySend.findByTaskidAndEmployeeid", query = "SELECT s FROM SalarySend s WHERE s.salarySendPK.taskid =:taskid AND s.salarySendPK.employeeid=:employeeid"),
-    @NamedQuery(name = "SalarySend.findByTaskid", query = "SELECT s FROM SalarySend s WHERE s.salarySendPK.taskid = :taskid"),
+    @NamedQuery(name = "SalarySend.findByTaskidAndDeptno", query = "SELECT s FROM SalarySend s WHERE s.taskid like :taskid AND s.deptno = :deptno"),
+    @NamedQuery(name = "SalarySend.findByTaskidAndEmployeeid", query = "SELECT s FROM SalarySend s WHERE s.taskid =:taskid AND s.employeeid=:employeeid"),
+    @NamedQuery(name = "SalarySend.findById", query = "SELECT s FROM SalarySend s WHERE s.id = :id"), 
+    @NamedQuery(name = "SalarySend.findByTaskid", query = "SELECT s FROM SalarySend s WHERE s.taskid = :taskid"),
     @NamedQuery(name = "SalarySend.findByTaskname", query = "SELECT s FROM SalarySend s WHERE s.taskname = :taskname"),
     @NamedQuery(name = "SalarySend.findBySendtime", query = "SELECT s FROM SalarySend s WHERE s.sendtime = :sendtime"),
-    @NamedQuery(name = "SalarySend.findByEmployeeid", query = "SELECT s FROM SalarySend s WHERE s.salarySendPK.employeeid = :employeeid"),
+    @NamedQuery(name = "SalarySend.findByEmployeeid", query = "SELECT s FROM SalarySend s WHERE s.employeeid = :employeeid"),
     @NamedQuery(name = "SalarySend.findByEmployeename", query = "SELECT s FROM SalarySend s WHERE s.employeename = :employeename"),
     @NamedQuery(name = "SalarySend.findByDeptno", query = "SELECT s FROM SalarySend s WHERE s.deptno = :deptno"),
     @NamedQuery(name = "SalarySend.findByDept", query = "SELECT s FROM SalarySend s WHERE s.dept = :dept"),
     @NamedQuery(name = "SalarySend.findByStatus", query = "SELECT s FROM SalarySend s WHERE s.status = :status"),
-    @NamedQuery(name = "SalarySend.findByConfirmtime", query = "SELECT s FROM SalarySend s WHERE s.confirmtime = :confirmtime")})
-public class SalarySend implements Serializable {
+    @NamedQuery(name = "SalarySend.findByConfirmtime", query = "SELECT s FROM SalarySend s WHERE s.confirmtime = :confirmtime"),
+    @NamedQuery(name = "SalarySend.findByCfmdate", query = "SELECT s FROM SalarySend s WHERE s.cfmdate = :cfmdate"),
+    @NamedQuery(name = "SalarySend.findByOptuser", query = "SELECT s FROM SalarySend s WHERE s.optuser = :optuser"),
+    @NamedQuery(name = "SalarySend.findByOptdate", query = "SELECT s FROM SalarySend s WHERE s.optdate = :optdate"),
+    @NamedQuery(name = "SalarySend.findByCfmuser", query = "SELECT s FROM SalarySend s WHERE s.cfmuser = :cfmuser"),
+    @NamedQuery(name = "SalarySend.findByCredate", query = "SELECT s FROM SalarySend s WHERE s.credate = :credate"),
+    @NamedQuery(name = "SalarySend.findByCreator", query = "SELECT s FROM SalarySend s WHERE s.creator = :creator")})
+public class SalarySend extends SuperEntity {
 
     private static final long serialVersionUID = 1L;
-    @EmbeddedId
-    protected SalarySendPK salarySendPK;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "taskid")
+    private String taskid;
     @Size(max = 20)
     @Column(name = "taskname")
     private String taskname;
     @Column(name = "sendtime")
     @Temporal(TemporalType.TIMESTAMP)
     private Date sendtime;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "employeeid")
+    private String employeeid;
     @Size(max = 8)
     @Column(name = "employeename")
     private String employeename;
@@ -58,9 +74,6 @@ public class SalarySend implements Serializable {
     @Size(max = 20)
     @Column(name = "dept")
     private String dept;
-    @Size(max = 1)
-    @Column(name = "status")
-    private String status;
     @Column(name = "confirmtime")
     @Temporal(TemporalType.TIMESTAMP)
     private Date confirmtime;
@@ -68,20 +81,23 @@ public class SalarySend implements Serializable {
     public SalarySend() {
     }
 
-    public SalarySend(SalarySendPK salarySendPK) {
-        this.salarySendPK = salarySendPK;
+    public SalarySend(Integer id) {
+        this.id = id;
     }
 
-    public SalarySend(String taskid, String employeeid) {
-        this.salarySendPK = new SalarySendPK(taskid, employeeid);
+    public SalarySend(Integer id, String taskid, String employeeid) {
+        this.id = id;
+        this.taskid = taskid;
+        this.employeeid = employeeid;
     }
 
-    public SalarySendPK getSalarySendPK() {
-        return salarySendPK;
+
+    public String getTaskid() {
+        return taskid;
     }
 
-    public void setSalarySendPK(SalarySendPK salarySendPK) {
-        this.salarySendPK = salarySendPK;
+    public void setTaskid(String taskid) {
+        this.taskid = taskid;
     }
 
     public String getTaskname() {
@@ -98,6 +114,14 @@ public class SalarySend implements Serializable {
 
     public void setSendtime(Date sendtime) {
         this.sendtime = sendtime;
+    }
+
+    public String getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(String employeeid) {
+        this.employeeid = employeeid;
     }
 
     public String getEmployeename() {
@@ -139,22 +163,20 @@ public class SalarySend implements Serializable {
     public void setConfirmtime(Date confirmtime) {
         this.confirmtime = confirmtime;
     }
-
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (salarySendPK != null ? salarySendPK.hashCode() : 0);
+        hash += (id != null ? id.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof SalarySend)) {
             return false;
         }
         SalarySend other = (SalarySend) object;
-        if ((this.salarySendPK == null && other.salarySendPK != null) || (this.salarySendPK != null && !this.salarySendPK.equals(other.salarySendPK))) {
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
         return true;
@@ -162,7 +184,7 @@ public class SalarySend implements Serializable {
 
     @Override
     public String toString() {
-        return "cn.hanbell.wco.entity.SalarySend[ salarySendPK=" + salarySendPK + " ]";
+        return "cn.hanbell.eap.entity.Salarysend[ id=" + id + " ]";
     }
     
 }
