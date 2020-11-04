3b06e9f91ae5de5eedb8f2b8de8a090762149bf3
C1749 <C1749@hanbell.com.cn>
2020-11-04 16:10:25 +0800

    update 免费服务金额明细表功能的方法名
    getERPTrafficamtAmt()改为getERPTrafficAmt()

--- SHBERP-ejb/src/java/cn/hanbell/erp/ejb/CdrlnhadBean.java
+++ SHBERP-ejb/src/java/cn/hanbell/erp/ejb/CdrlnhadBean.java
@@ -384,7 +384,7 @@ public class CdrlnhadBean extends SuperEJBForERP<Cdrlnhad> {
      * @param fwno
      * @return CDRN20借出归还单运费
      */
-    public BigDecimal getERPTrafficamtAmt(String fwno) {
+    public BigDecimal getERPTrafficAmt(String fwno) {
         StringBuilder sb = new StringBuilder();
         BigDecimal amt;
         sb.append(" select isnull(sum(c.freight),0) as amt from cdrlnhad h LEFT JOIN cdrfre c on c.shpno = h.trno and c.facno = h.facno ");
