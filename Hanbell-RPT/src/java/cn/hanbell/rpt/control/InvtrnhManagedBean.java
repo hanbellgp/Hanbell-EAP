531c9f11292b1e0679ed6fe5a2a4cff45d4693a7
C2082 <shenxin@hanebll.com.cn>
2021-03-18 16:25:24 +0800

    INV555库存异动报表新增本月异动单据

--- Hanbell-RPT/src/java/cn/hanbell/rpt/control/InvtrnhManagedBean.java
+++ Hanbell-RPT/src/java/cn/hanbell/rpt/control/InvtrnhManagedBean.java
@@ -7,11 +7,14 @@ package cn.hanbell.rpt.control;
 
 import cn.hanbell.erp.ejb.CdrcusBean;
 import cn.hanbell.erp.ejb.InvhdscBean;
+import cn.hanbell.erp.ejb.InvsysBean;
+import cn.hanbell.erp.ejb.InvtrnBean;
 import cn.hanbell.erp.ejb.InvtrnhBean;
 import cn.hanbell.erp.ejb.MiscodeBean;
 import cn.hanbell.erp.ejb.MisdeptBean;
 import cn.hanbell.erp.entity.Cdrcus;
 import cn.hanbell.erp.entity.Invhdsc;
+import cn.hanbell.erp.entity.Invsys;
 import cn.hanbell.erp.entity.Invtrnh;
 import cn.hanbell.erp.entity.Miscode;
 import cn.hanbell.erp.entity.Misdept;
@@ -24,10 +27,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import org.apache.poi.ss.usermodel.Cell;
@@ -55,6 +61,10 @@ public class InvtrnhManagedBean extends SuperQueryBean<Invtrnh> {
     private InvhdscBean invhdscBean;
     @EJB
     private CdrcusBean cdrcusBean;
+    @EJB
+    private InvsysBean invsysBean;
+    @EJB
+    private InvtrnBean invtrnBean;
     private String queryfacno;
     private String queryno;
     private String querytype;
@@ -62,7 +72,7 @@ public class InvtrnhManagedBean extends SuperQueryBean<Invtrnh> {
     private String querydept;
     private String queryuser;
     private String facnoView;
-    private List<Invtrnh> list;
+    private List<Object[]> list;
 
     public InvtrnhManagedBean() {
         super(Invtrnh.class);
@@ -101,17 +111,44 @@ public class InvtrnhManagedBean extends SuperQueryBean<Invtrnh> {
         miscodeBean.setCompany("C");
         invhdscBean.setCompany("C");
         cdrcusBean.setCompany("C");
+        invsysBean.setCompany("C");
+        invtrnBean.setCompany("C");
     }
 
     @Override
     public void query() {
-        this.facnoView = invtrnhBean.getCompanyName(this.queryfacno);
-        invtrnhBean.setCompany(queryfacno);
-        misdeptBean.setCompany(queryfacno);
-        miscodeBean.setCompany(queryfacno);
-        invhdscBean.setCompany(queryfacno);
-        cdrcusBean.setCompany(queryfacno);
-        list = invtrnhBean.getInvtrnhByINV555(queryfacno, queryDateBegin, queryDateEnd, changeVlaue(queryno), changeVlaue(querytype), changeVlaue(querywareh), changeVlaue(querydept), changeVlaue(queryuser));
+        try {
+            this.facnoView = invtrnhBean.getCompanyName(this.queryfacno);
+            invtrnhBean.setCompany(queryfacno);
+            misdeptBean.setCompany(queryfacno);
+            miscodeBean.setCompany(queryfacno);
+            invhdscBean.setCompany(queryfacno);
+            cdrcusBean.setCompany(queryfacno);
+            invsysBean.setCompany(queryfacno);
+            invtrnBean.setCompany("C");
+            Invsys invsys = invsysBean.findByFacno(queryfacno);
+            Date monDate = BaseLib.getDate("yyyyMM", invsys.getLmonth());
+            Calendar c = Calendar.getInstance();
+            c.setTime(monDate);
+            c.add(Calendar.MONTH, 1);
+            //查询结束的时间大于月结的一个月时间并且数据两个开始年月和结束年月不同。则分开查询合并。
+            if(c.getTime().getTime()<=queryDateEnd.getTime()&&queryDateBegin.getTime()<c.getTime().getTime()){
+                c.add(Calendar.DAY_OF_MONTH,-1);
+                list = invtrnhBean.getInvtrnhByINV555(queryfacno, queryDateBegin, c.getTime(), changeVlaue(queryno), changeVlaue(querytype), changeVlaue(querywareh), changeVlaue(querydept), changeVlaue(queryuser));
+                List<Object[]> o=invtrnBean.getInvtrnhByINV555(facnoView, c.getTime(), queryDateEnd, changeVlaue(queryno), changeVlaue(querytype), changeVlaue(querywareh), changeVlaue(querydept), changeVlaue(queryuser));
+                list.addAll(o);
+            }else if(c.getTime().getTime()<=queryDateBegin.getTime() &&c.getTime().getTime()<=queryDateEnd.getTime()){
+                c.add(Calendar.DAY_OF_MONTH,-1);
+                list=invtrnBean.getInvtrnhByINV555(facnoView, c.getTime(), queryDateEnd,  changeVlaue(queryno), changeVlaue(querytype), changeVlaue(querywareh), changeVlaue(querydept), changeVlaue(queryuser));
+            }else{
+                list = invtrnhBean.getInvtrnhByINV555(queryfacno, queryDateBegin, queryDateEnd, changeVlaue(queryno), changeVlaue(querytype), changeVlaue(querywareh), changeVlaue(querydept), changeVlaue(queryuser));
+            }
+        } catch (ParseException ex) {
+            Logger.getLogger(InvtrnhManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+        System.out.println("");
     }
 
     @Override
@@ -140,7 +177,7 @@ public class InvtrnhManagedBean extends SuperQueryBean<Invtrnh> {
                 Row row = null;
                 int i = 1;
                 //打印单头档
-                for (Invtrnh h : list) {
+                for (Object[] h : list) {
                     row = sheet.createRow(i);
                     Cell cell = row.createCell(0);
                     cell.setCellValue(i);
@@ -191,33 +228,33 @@ public class InvtrnhManagedBean extends SuperQueryBean<Invtrnh> {
                     cell21.setCellStyle(cellStyle);
                     cell22.setCellStyle(cellStyle);
                     cell23.setCellStyle(cellStyle);
-                    cell1.setCellValue(h.getTrdate() != null ? BaseLib.formatDate("yyyy/MM/dd", h.getTrdate()) : "");
-                    cell2.setCellValue(h.getInvdou() != null ? h.getInvdou().getTrtype() : "");
-                    cell3.setCellValue(h.getInvdou() != null ? h.getInvdou().getTypedsc() : "");
-                    cell4.setCellValue(h.getInvtrnhPK().getFacno() != null ? h.getInvtrnhPK().getFacno() : "");
-                    cell5.setCellValue(h.getInvtrnhPK().getProno() != null ? h.getInvtrnhPK().getProno() : "");
-                    cell6.setCellValue(h.getDepno() != null ? h.getDepno() : "");
-                    Misdept m = misdeptBean.findByDepno(h.getDepno());
+                    cell1.setCellValue(h[6] != null ? BaseLib.formatDate("yyyy/MM/dd", (Date) h[6]) : "");
+                    cell2.setCellValue(h[0] != null ? (String) h[0] : "");
+                    cell3.setCellValue(h[1] != null ? (String) h[1] : "");
+                    cell4.setCellValue(h[2] != null ? (String) h[2] : "");
+                    cell5.setCellValue(h[3] != null ? (String) h[3] : "");
+                    cell6.setCellValue(h[4] != null ? (String) h[4] : "");
+                    Misdept m = misdeptBean.findByDepno((String) h[4]);
                     if (m == null) {
-                        Cdrcus cdrcus = cdrcusBean.findByCusno(h.getDepno());
+                        Cdrcus cdrcus = cdrcusBean.findByCusno((String) h[4]);
                         cell7.setCellValue(cdrcus != null ? cdrcus.getCusna() : "");
                     } else {
                         cell7.setCellValue(m != null ? m.getDepname() : "");
                     }
-                    cell8.setCellValue(h.getInvtrnhPK().getTrno() != null ? h.getInvtrnhPK().getTrno() : "");
-                    cell9.setCellValue(String.valueOf(h.getInvtrnhPK().getTrseq()));
-                    cell10.setCellValue(h.getItnbr() != null ? h.getItnbr() : "");
-                    cell11.setCellValue(h.getInvmas() != null ? h.getInvmas().getItdsc() : "");
-                    cell12.setCellValue(h.getInvmas() != null && h.getInvmas().getInvcls() != null ? h.getInvmas().getInvcls().getItcls() : "");
-                    cell13.setCellValue(h.getInvmas() != null && h.getInvmas().getInvcls() != null ? h.getInvmas().getInvcls().getClsdsc() : "");
-                    cell14.setCellValue(h.getWareh() != null ? h.getWareh() : "");
-                    cell15.setCellValue(h.getInvwh() != null ? h.getInvwh().getWhdsc() : "");
-                    cell16.setCellValue(h.getTrnqy1() != null ? String.valueOf(h.getTrnqy1()) : "");
-                    cell17.setCellValue(h.getUnmsr1() != null ? String.valueOf(h.getUnmsr1()) : "");
-                    cell18.setCellValue(h.getTramt() != null ? String.valueOf(h.getTramt()) : "");
-                    cell19.setCellValue(h.getUserno() != null ? h.getUserno() : "");
-                    cell20.setCellValue(h.getIocode() != null ? h.getIocodeValue(h.getIocode()) : "");
-                    Invhdsc invdsc = invhdscBean.findByFacnoAndPronoAndTrno(h.getInvtrnhPK().getFacno(), h.getInvtrnhPK().getProno(), h.getInvtrnhPK().getTrno());
+                    cell8.setCellValue(h[6] != null ? BaseLib.formatDate("yyyy/MM/dd", (Date) h[6]) : "");
+                    cell9.setCellValue(String.valueOf((int) h[7]));
+                    cell10.setCellValue(h[8] != null ? (String) h[8] : "");
+                    cell11.setCellValue(h[9] != null ? (String) h[9] : "");
+                    cell12.setCellValue(h[10] != null ? (String) h[10] : "");
+                    cell13.setCellValue(h[11] != null ? (String) h[11] : "");
+                    cell14.setCellValue(h[12] != null ? (String) h[12] : "");
+                    cell15.setCellValue(h[13] != null ? (String) h[13] : "");
+                    cell16.setCellValue(h[14] != null ? String.valueOf((java.math.BigDecimal) h[14]) : "");
+                    cell17.setCellValue(h[15] != null ? (String) h[15] : "");
+                    cell18.setCellValue(h[16] != null ? String.valueOf((java.math.BigDecimal) h[16]) : "");
+                    cell19.setCellValue(h[17] != null ? (String) h[17] : "");
+                    cell20.setCellValue(h[18] != null ? (String) h[18] : "");
+                    Invhdsc invdsc = invhdscBean.findByFacnoAndPronoAndTrno((String) h[2], (String) h[3], (String) h[5]);
                     StringBuffer invdscBuffer = new StringBuffer(";;");
                     if (invdsc != null) {
                         if (invdsc.getMark1() != null && !"".equals(invdsc.getMark1())) {
@@ -235,8 +272,8 @@ public class InvtrnhManagedBean extends SuperQueryBean<Invtrnh> {
                         invdscBuffer.append(";;");
                         cell21.setCellValue(invdscBuffer.toString());
                     }
-                    Miscode miscode = miscodeBean.findByPK("IK", h.getRescode());
-                    cell22.setCellValue(h.getRescode() != null ? "IK" : "");
+                    Miscode miscode = miscodeBean.findByPK((String) h[19], (String) h[20]);
+                    cell22.setCellValue(h[19] != null ? getIocodeValue((String) (h[19])) : "");
                     cell23.setCellValue(miscode != null ? miscode.getCusds() : "");
                     i++;
                 }
@@ -273,11 +310,11 @@ public class InvtrnhManagedBean extends SuperQueryBean<Invtrnh> {
         }
     }
 
-    public List<Invtrnh> getList() {
+    public List<Object[]> getList() {
         return list;
     }
 
-    public void setList(List<Invtrnh> list) {
+    public void setList(List<Object[]> list) {
         this.list = list;
     }
 
@@ -347,6 +384,22 @@ public class InvtrnhManagedBean extends SuperQueryBean<Invtrnh> {
             sb.append("'").append(nos[i]).append("',");
         }
         return sb.substring(0, sb.length() - 1);
+    }
+
+    public String getIocodeValue(String ioccode) {
+        switch (ioccode) {
+            case "0":
+                return "期初";
+            case "1":
+                return "入库";
+            case "2":
+                return "出库";
+            case "3":
+                return "调拨";
+            case "Z":
+                return "期末";
+        }
+        return "";
     }
 
 }
--- SHBERP-ejb/src/java/cn/hanbell/erp/ejb/InvtrnBean.java
+++ SHBERP-ejb/src/java/cn/hanbell/erp/ejb/InvtrnBean.java
@@ -7,8 +7,12 @@ package cn.hanbell.erp.ejb;
 
 import cn.hanbell.erp.comm.SuperEJBForERP;
 import cn.hanbell.erp.entity.Invtrn;
+import cn.hanbell.util.BaseLib;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.persistence.Query;
 
 /**
  *
@@ -22,4 +26,40 @@ public class InvtrnBean extends SuperEJBForERP<Invtrn> {
         super(Invtrn.class);
     }
 
+    public List<Object[]> getInvtrnhByINV555(String facno, Date queryDateBegin, Date queryDateEnd, String queryno, String querytype, String querywareh, String querydept, String queryuser) {
+        StringBuffer sql = new StringBuffer();
+        sql.append("SELECT invtrn.trtype,invdou.typedsc,invtrn.facno,invtrn.prono,invtrn.depno, invtrn.trno,invtrn.trdate,invtrn.trseq, invtrn.itnbr,");
+        sql.append(" invmas.itdsc,invcls.itcls,invcls.clsdsc, invtrn.wareh,invwh.whdsc, invtrn.trnqy1,invtrn.unmsr1,invtrn.tramt,invtrn.userno, invtrn.iocode,invdou.reskind,invtrn.rescode");
+        sql.append(" FROM invtrn, invmas, invwh, invdou, invcls");
+        sql.append(" WHERE (invmas.itnbr = invtrn.itnbr) AND(invtrn.facno = invwh.facno) AND (invtrn.prono = invwh.prono) AND (invtrn.wareh = invwh.wareh) AND");
+        sql.append(" (invdou.trtype = invtrn.trtype) AND (invcls.itcls = invmas.itcls) AND ((invdou.syscode = '10') AND (invdou.reskind IS NOT NULL AND");
+        sql.append(" ltrim(invdou.reskind) <> '') AND (invdou.iocode IN ('1', '2') OR invdou.iocode = '3' AND invdou.trtype IN (SELECT trntp FROM cstrul WHERE facno = invtrn.facno AND avgco = 'Y')))");
+        sql.append(" AND (invtrn.facno = '").append(facno).append("'").append(" AND invtrn.prono = '1' AND (invdou.depdsckind IN (SELECT ckind  FROM misckind)) AND");
+        sql.append(" (invtrn.trdate > '").append(BaseLib.formatDate("yyyy/MM/dd", queryDateBegin)).append("' and");
+        sql.append(" invtrn.trdate <='").append(BaseLib.formatDate("yyyy/MM/dd", queryDateEnd)).append("')");
+
+        if (!"".equals(queryno) && queryno != null) {
+            sql.append(" and invtrn.trno in (").append(queryno).append(")");
+        }
+        if (!"".equals(querytype) && querytype != null) {
+            sql.append(" and invtrn.trtype in (").append(querytype).append(")");
+        }
+        if (!"".equals(querywareh) && querywareh != null) {
+            sql.append(" and invtrn.wareh in (").append(querywareh).append(")");
+        }
+        if (!"".equals(querydept) && querydept != null) {
+            sql.append(" and invtrn.depno in (").append(querydept).append(")");
+        }
+        if (!"".equals(queryuser) && queryuser != null) {
+            sql.append(" and invtrn.userno in (").append(queryuser).append(")");
+        }
+        sql.append(")");
+        try {
+            Query query = getEntityManager().createNativeQuery(sql.toString());
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
